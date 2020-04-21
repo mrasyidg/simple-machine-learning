e95f6680b007ed8a361a196fbc89cc52b9a22ebc
Rasyid Gatrawijaya <rasyid.gatrawijaya@gmail.com>
2020-04-22 03:23:45 +0700

    tidy up some lines of code

--- regression-algorithms/linear-regression/LinearRegression.py
+++ regression-algorithms/linear-regression/LinearRegression.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 def cost_fun(X,y,w): # w is theta
-    m=len(y)
+    m = len(y)
     predictions = X.dot(w)
     square_error = (predictions - y) ** 2
     
     return 1/( 2 * m) * np.sum(square_error)
 
-def gradient_descent( X, y, w, alpha, num_iters): # w is theta
-    m=len(y)
-    J_history=[]
+def gradient_descent(X, y, w, alpha, num_iters): # w is theta
+    m = len(y)
+    J_history = []
     
     for i in range(num_iters):
         predictions = X.dot(w)
-        error = np.dot(X.transpose(),(predictions -y))
+        error = np.dot( X.transpose(),(predictions - y))
         descent = alpha * 1 / m * error
         theta -= descent
         J_history.append(cost_fun(X,y,w))
