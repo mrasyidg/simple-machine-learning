958189ae9c779e9a810af4deea8d1a08c0bd8964
Rasyid <rasyid.gatrawijaya@gmail.com>
2020-05-26 01:35:54 +0700

    Add stable softmax and (single) Dense layer classes

---
+++ artificial-neural-network-algorithms/artificial-neural-network-python-implementation/Dense.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+class Dense(Layer):
+    
+    def __init__(self, input_shape, output_shape, weight_initializer='random'):
+
+        if weight_initializer not in ['xavier','random']:
+            raise ValueError('weight_initializer must be either xavier or random')
+
+        if weight_initializer == 'random':
+            self.weights = np.random.randn(input_shape, output_shape)*1e-2
+        
+        elif weight_initializer == 'xavier':
+            self.weights = np.random.randn(input_shape, output_shape)*np.sqrt(2/(input_shape + output_shape))
+        
+        self.bias = np.random.randn(output_shape)
+    
+    def forward(self, _input):
+        return np.dot(_input, self.weights) + self.bias
+        raise NotImplementedError()
+        
+    def backward(self, _input, grad_output):
+        grad_input = np.dot(grad_output, np.transpose(self.weights))
+        grad_weights = np.dot(np.transpose(_input), grad_output)
+        grad_bias = grad_output.mean(axis=0)*_input.shape[0]
+        return [grad_input, grad_weights, grad_bias]
+
+
+"""
+You can test it with these statements:
+
+np.random.seed(3)
+dense = Dense(2,2)
+np.testing.assert_almost_equal(dense.forward(np.ones([3,2])), np.array([[-0.25853694, -0.36902881], [-0.25853694, -0.36902881],[-0.25853694, -0.36902881]]))
+
+dense.weights = np.ones((2,2))
+dense.bias = np.zeros(2)
+backprop = dense.backward(np.array([[1,1]]), np.array([[1,1]]))
+np.testing.assert_almost_equal(backprop[0], np.array([[2., 2.]]))
+np.testing.assert_almost_equal(backprop[1], np.array([[1., 1.], [1., 1.]]))
+np.testing.assert_almost_equal(backprop[2], np.array([1., 1.]))
+
+assert dense.weights.shape == backprop[1].shape
+assert dense.weights.shape[0] == backprop[0].shape[1]
+"""
---
+++ artificial-neural-network-algorithms/artificial-neural-network-python-implementation/Softmax.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class Softmax(Layer):
+    
+    def __init__(self):
+        pass
+
+    def forward(self, _input):
+        softmax = lambda x: np.exp(x-np.max(x))/np.sum(np.exp(x-np.max(x)))
+        return[softmax(i) for i in _input]
+
+    def backward(self, _input, grad_output):
+        return [grad_output]
+
+"""
+You can test it with these statements:
+softmax = Softmax()
+np.testing.assert_almost_equal(softmax.forward(np.array([[100],[1],[0]])), np.array([[1],[1],[1]]))
+np.testing.assert_almost_equal(softmax.forward(np.array([[100,3000],[1,1],[0,1]])), np.array([[0,1],[0.5,0.5],[0.2689414, 0.7310586]]))
+"""
