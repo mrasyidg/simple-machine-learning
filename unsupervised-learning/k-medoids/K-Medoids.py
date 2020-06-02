379785b7e75b1c8b2cab5080c74d81585394e621
Rasyid <rasyid.gatrawijaya@gmail.com>
2020-06-02 20:01:38 +0700

    Update on k-medoids.

--- unsupervised-learning/k-medoids/K-Medoids.py
+++ unsupervised-learning/k-medoids/K-Medoids.py
@@ -18,58 +18,77 @@ class Kmedoids:
     def euclidean_distance(self, x1, x2):
         distance = 0
 
-        raise NotImplementedError()
+        n = len(x1)
+        inside = 0
+        for i in range(n):
+            inside += ((x1[i]-x2[i])**2)
+        distance = math.sqrt(inside)
 
-        # return distance
+        return distance
 
     def compute_distance_matrix(self, X, rep):
         r = len(X)
         c = len(rep)
         S = np.empty((r, c))
         
-        raise NotImplementedError()
+        for i in range(r):
+            for j in range(c):
+                S[i][j] = self.euclidean_distance(X[i], rep[j])
+        return S
 
     def assign_label(self, S):
 
-        raise NotImplementedError()
+        r = len(S)
+        res = np.empty(r)
+        for i in range(r):
+            res[i] = int(np.argmin(S[i]))
+        return res.astype(int)
 
     def calculate_cost(self, S):
 
-        raise NotImplementedError()
+        res = []
+        for i in range(len(S)):
+            res.append(min(S[i]))
+        return np.sum(res)
 
     def update_medoids(self, X, cost):
         best_medoids = self.initial_medoids
         lowest_cost = cost
-        while True:
-            # iterate until we get the medoid with the lowest cost
+        for i in range(100):
 
-            # raise NotImplementedError()
-            
-            if lowest_cost < cost:
-                cost = lowest_cost
-                medoids = best_medoids
+            # iterate until we get the medoid with the lowest cost
+            random_idx = np.random.choice(X.shape[0], size=self.n_clusters, replace=False)
+            medoids = X[random_idx, :]
+
+            S = compute_distance_matrix(X, medoids)
+            labels = assign_label(S)
+            cost = calculate_cost(S)
+
+            if lowest_cost > cost:
+                lowest_cost = cost
+                best_medoids = medoids
             else:
-                break
+                continue
 
         print('Best medoids: ', best_medoids)
         print('Lowest cost: ', lowest_cost)
-
         return best_medoids, lowest_cost
 
     def fit(self, X):
         self.initial_medoids = self.initialize_medoids(X)
-        
-        raise NotImplementedError()
-        
+
+        dist_mtx = self.compute_distance_matrix(X, self.initial_medoids)
+        cost = self.calculate_cost(dist_mtx)
+        self.best_medoids, self.lowest_cost = self.update_medoids(X, cost)
+
         self.labels = assign_label(self.compute_distance_matrix(X, self.best_medoids))
+        return self
 
     def predict(self, X):
         labels = []
-
-        raise NotImplementedError()
-
-        # return labels
-
-def assign_label(S):
-    
-    raise NotImplementedError()
+        
+        dist_mtx = self.compute_distance_matrix(X, self.best_medoids)
+        labels = self.assign_label(dist_mtx)
+        print(dist_mtx)
+        print(labels)
+        return labels
